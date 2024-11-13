9943944c0eceda82400fc03ebb880ac37510f47b
Joshua Park <sp72@calvin.edu>
2024-11-12 22:42:43 -0500

    added delete button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -269,9 +269,8 @@ function DryerScreen(){
   );
 }
 
-
 function NotificationScreen() {
-  const { showLaundryButton } = useNotification();
+  const { showLaundryButton, setShowLaundryButton } = useNotification();
 
   return (
     <View style={styles.notificationContainer}>
@@ -284,16 +283,18 @@ function NotificationScreen() {
  
       </View>
       {showLaundryButton && (
-        <TouchableOpacity 
-          style={styles.buttonBox} 
-          onPress={() => alert('Laundry Machine 1 selected')}>
+        <View style={styles.buttonBox}>
           <View style={styles.buttonContent}>
             <View style={styles.textContainer}>
               <Text style={styles.buttonHeading}>Laundry Machine 1</Text>
+              <TouchableOpacity style={styles.deleteButton} onPress={() => setShowLaundryButton(false)}>
+                <Text style={styles.deleteButtonText}>Delete</Text>
+              </TouchableOpacity>
             </View>
             <Image source={LaundryIcon} style={styles.icon} />
           </View>
-        </TouchableOpacity>
+  
+        </View>
       )}
     </View>
   );
@@ -518,6 +519,23 @@ notifIcon: {
   width: 60,
   height: 60,
   marginRight: 20,
-}
+},
+
+/* Delete Button */
+
+deleteButton: {
+  backgroundColor: '#FF6B6B',
+  padding: 10,
+  borderRadius: 10,
+  width: '50%',
+  alignItems: 'center',
+  marginTop: 15,
+},
+
+deleteButtonText: {
+  color: '#FFF',
+  fontSize: 12,
+  fontWeight: 'bold',
+},
 
 });
